f4b8912af3bcf98be5f2587e87e7827415e95969
Anamika Pandey <83291071+anamika62@users.noreply.github.com>
2021-12-04 20:04:51 +0530

    Add files via upload

---
+++ Call.js
@@ -0,0 +1,12 @@
+console.log('THis is my calucuLaToR');
+function display(val) {
+  document.getElementById('result').value += val;
+}
+function solve() {
+  let x = document.getElementById('result').value;
+  let y = eval(x);
+  document.getElementById('result').value = y;
+}
+function clearScreen() {
+  document.getElementById('result').value = '';
+}
